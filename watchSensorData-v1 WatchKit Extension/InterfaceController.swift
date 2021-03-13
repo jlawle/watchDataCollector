f55273b3e195d0cd0066b317d98ba638c9c07eb6
John Lawler <lawler6@g.clemson.edu>
2021-03-13 14:07:51 -0500

    Initial Commit

---
+++ watchSensorData-v1 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  watchSensorData-v1 WatchKit Extension
+//
+//  Created by John Lawler on 3/13/21.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
